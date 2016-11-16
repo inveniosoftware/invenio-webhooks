ad01162c8b28b63ad126b25d5c72b07c36551ff8
Leonardo Rossi <leonardo.r@cern.ch>
2016-11-16 17:46:44 +0100

    models: fix json fields
    Signed-off-by: Leonardo Rossi <leonardo.r@cern.ch>

--- invenio_webhooks/models.py
+++ invenio_webhooks/models.py
@@ -37,6 +37,7 @@ from invenio_db import db
 from sqlalchemy.dialects import postgresql
 from sqlalchemy.orm import validates
 from sqlalchemy.orm.attributes import flag_modified
+from sqlalchemy.types import JSON
 from sqlalchemy_utils.models import Timestamp
 from sqlalchemy_utils.types import JSONType, UUIDType
 
@@ -198,10 +199,9 @@ class CeleryReceiver(Receiver):
 def _json_column(**kwargs):
     """Return JSON column."""
     return db.Column(
-        JSONType().with_variant(
-            postgresql.JSON(none_as_null=True),
-            'postgresql',
-        ),
+        JSON().with_variant(
+            postgresql.JSONB(none_as_null=True), 'postgresql'
+        ).with_variant(JSONType(), 'sqlite'),
         nullable=True,
         **kwargs
     )
