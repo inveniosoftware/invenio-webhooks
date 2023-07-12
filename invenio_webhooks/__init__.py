156180ab4459fde3d197f9d64d7ad43f75bc86d6
Zacharias Zacharodimos <zacharias.zacharodimos@cern.ch>
2023-07-12 10:14:58 +0200

    release: v1.0.1

--- invenio_webhooks/__init__.py
+++ invenio_webhooks/__init__.py
@@ -28,7 +28,7 @@ from .ext import InvenioWebhooks
 from .models import Receiver
 from .proxies import current_webhooks
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 __all__ = (
     "__version__",
