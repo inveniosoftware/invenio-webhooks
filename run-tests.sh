3676c7aadd6f82cb5d88d798013e690360c1557a
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-06-30 13:47:44 +0200

    chore(setup): migrate from setuptools to hatchling
    * remove MANIFEST.in because that's only relevant for setuptools
    * translate the i18n/translation mechanisms as well
    * format pyproject.toml with Tombi
    
    Co-authored-by: Sam Arbid <samaa2@kth.se>

--- run-tests.sh
+++ run-tests.sh
@@ -20,7 +20,6 @@ function cleanup() {
 trap cleanup EXIT
 
 
-python -m check_manifest --ignore ".*-requirements.txt"
 python -m sphinx.cmd.build -qnN --keep-going docs docs/_build/html
 eval "$(docker-services-cli up --db ${DB:-postgresql} --cache ${CACHE:-redis} --env)"
 python -m pytest
